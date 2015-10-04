8f005c6212ee1ac6bfde12a1daa45a344c63455d
mo khan <mo@mokhan.ca>
2015-10-04 16:51:26 -0600

    update yum and apt packages.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,11 @@
+if redhat?
+  execute "yum update -y"
+else
+  execute "apt-get update -y"
+end
+
 package node['chef-talk']['packages']
+
 include_recipe "chef-talk::ntpd"
 include_recipe "chef-talk::nginx"
 include_recipe "chef-talk::ruby"
--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe "chef-talk::default" do
   subject do
-    ChefSpec::SoloRunner.new.converge(described_recipe)
+    ChefSpec::SoloRunner.new(configuration).converge(described_recipe)
   end
+
+  let(:configuration) { {} }
 
   it 'installs ruby' do
     expect(subject).to include_recipe('chef-talk::ruby')
@@ -19,5 +21,21 @@ describe "chef-talk::default" do
 
   it 'installs ntpd' do
     expect(subject).to include_recipe('chef-talk::ntpd')
+  end
+
+  context 'centos' do
+    let(:configuration) { { platform: 'centos', version: '6.6' } }
+
+    it 'updates the package index' do
+      expect(subject).to run_execute('yum update -y')
+    end
+  end
+
+  context 'ubuntu' do
+    let(:configuration) { { platform: 'ubuntu', version: '14.04' } }
+
+    it 'updates the package index' do
+      expect(subject).to run_execute('apt-get update -y')
+    end
   end
 end
